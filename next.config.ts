5123eb53f46b4147fbd3cd08bae71814f5406d28
GuedesBruno <brunoguedes@tecassistiva.com.br>
2026-01-27 17:31:59 -0300

    fix: reset to default nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    unoptimized: true,
-  },
-  output: 'standalone',
 };
 
 export default nextConfig;
