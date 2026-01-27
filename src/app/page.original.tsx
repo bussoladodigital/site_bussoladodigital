5764778c09cae931d2ad8ee74209ba9de03a991d
GuedesBruno <brunoguedes@tecassistiva.com.br>
2026-01-27 17:51:15 -0300

    chore: debug minimal home page

---
+++ src/app/page.original.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: '#000', color: '#fff' }}>
+      <h1>Bússola do Digital - Teste Mínimo</h1>
+    </div>
+  );
+}
